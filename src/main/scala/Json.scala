14d49feeb506df8f65e7471d3ff4c27f6980fb0d
pablolopezgallego <36295104+pablolopezgallego@users.noreply.github.com>
2018-02-15 09:50:53 +0100

    Primera subida

--- src/main/scala/Json.scala
+++ src/main/scala/Json.scala
@@ -11,10 +11,10 @@ import org.apache.hadoop.hbase.util.Bytes
 import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.hadoop.hbase.spark.HBaseContext
 
-
 object Json {
   //  org.apache.log4j.BasicConfigurator.configure()
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]){
+
     /** EL código de spark conf para hacer el streaming */
     val conf = new SparkConf().setAppName("HBaseStream")
     if (sys.env("ENTORNO") == "DESARROLLO") {
@@ -24,7 +24,7 @@ object Json {
 
     /* KafkaConf tiene un Map de la ruta del server de kafka, la ruta del server de zookeeper, el grupo.id del consumidor para poder hacer redundancia, el timeout para conectar a zookeeper */
     val kafkaConf = Map("metadata.broker.list" -> "localhost:42111",
-      "zookeeper.connect" -> "51.255.74.114:21000",
+      "zookeeper.connect" -> "localhost:21000",
       "group.id" -> "kafka-example",
       "zookeeper.connection.timeout.ms" -> "1000",
       "zookeeper.session.timeout.ms" -> "10000")
@@ -43,16 +43,13 @@ object Json {
     val rutaTax = "file:///C:/Users/plopez/Desktop/Taxonomias.csv" //URL a identificador
     val camposTax = "file:///C:/Users/plopez/Desktop/DictTax.csv" //identificador a taxoniomía
     val camposGenerales = "file:///C:/Users/plopez/Desktop/dictVarSanitas.txt" //Campos que contiene el Json recibido
-
 */
-
     /* Versión en el servidor, en un futuro se pasará como argumentos */
     val rutaTax = "file:///Pablo/Taxonomias.csv" //URL a identificador
     val camposTax = "file:///Pablo/DictTax.csv" //identificador a taxoniomía
     val camposGenerales = "file:///Pablo/dictVarSanitas.txt" //Campos que contiene el Json recibido
     //    val rutaTrafico = args(3)    No se usa, la ruta es lo que leemos de kafka, seguramente sea el topic
     //    val destino = args(4)  Es HBase, aún desconocemos que meteremos al final, seguramente el HBase colum adn y tabla
-
 
     val tax = sc.textFile(rutaTax, 1)
     val taxFM = tax.map(x => (x.split(";")(0), x.split(";")(1)))
@@ -66,7 +63,8 @@ object Json {
 
     lines.print()
 
-    lines.foreachRDD{ k =>
+    lines.foreachRDD { k =>
+
       if (k.count() > 0) {
         /*Traducimos el Json a RDD */
         val traficoRDD = streamSqlContext.read.json(k).selectExpr(List("idTracker", "url") ++ camposGeneralesRDD.collect(): _*).rdd.keyBy(t => if (t.getAs[String]("url").indexOf('?') > 0) t.getAs[String]("url").substring(0, t.getAs[String]("url").indexOf('?')) else t.getAs[String]("url"))
@@ -89,31 +87,31 @@ object Json {
 
         var csv = traficoTax.map(l => (l._2._1, l._2._2))
 
-        traficoTax.collect().foreach(l => println { "traficoTax: " + l})
-        traficoRDD.collect().foreach(l => println {"traficoRDD: " + l})
-        csv.collect().foreach(l => println { "csv: " + l})
+        //        traficoTax.collect().foreach(l => println { "traficoTax: " + l})
+        //        traficoRDD.collect().foreach(l => println {"traficoRDD: " + l})
+        //        csv.collect().foreach(l => println {"csv: " + l})
 
-        val APP_NAME: String = "SparkHbaseJob"
-        var HBASE_DB_HOST: String = null
-        var HBASE_TABLE: String = null
-        var HBASE_COLUMN_FAMILY: String = null
-        HBASE_DB_HOST="127.0.0.1"
-        HBASE_TABLE="usuarios"
-        HBASE_COLUMN_FAMILY="adn"
-        val conf = HBaseConfiguration.create()
-        conf.set(TableInputFormat.INPUT_TABLE, HBASE_TABLE)
-        val connection = ConnectionFactory.createConnection(conf)
-        val table = connection.getTable(TableName.valueOf(Bytes.toBytes(HBASE_TABLE)))
+              val APP_NAME: String = "SparkHbaseJob"
+              var HBASE_DB_HOST: String = null
+              var HBASE_TABLE: String = null
+              var HBASE_COLUMN_FAMILY: String = null
+              HBASE_DB_HOST="127.0.0.1"
+              HBASE_TABLE="usuarios"
+              HBASE_COLUMN_FAMILY="adn"
+              val conf = HBaseConfiguration.create()
+              conf.set(TableInputFormat.INPUT_TABLE, HBASE_TABLE)
+              val connection = ConnectionFactory.createConnection(conf)
+              val table = connection.getTable(TableName.valueOf(Bytes.toBytes(HBASE_TABLE)))
 
-        csv.map(
-          (putRecord) => {
-            val put = new Put(Bytes.toBytes(putRecord._1(0).toString))
-            mapaVarGen.map(l=>
-              put.addColumn( Bytes.toBytes("adn"),  Bytes.toBytes(l._1), Bytes.toBytes(putRecord._1.getAs(l._1).toString))
-            )
-            put.addColumn( Bytes.toBytes("adn"),  Bytes.toBytes("idTax"+putRecord._2), Bytes.toBytes(putRecord._2))
-            table.put(put)
-          })
+              csv.map(
+                (putRecord) => {
+                  val put = new Put(Bytes.toBytes(putRecord._1(0).toString))
+//                  mapaVarGen.map(l=>
+//                    put.addColumn( Bytes.toBytes("adn"),  Bytes.toBytes(l._1), Bytes.toBytes(putRecord._1.getAs(l._1).toString))
+//                  )
+                  put.addColumn( Bytes.toBytes("adn"),  Bytes.toBytes("idTax"+putRecord._2), Bytes.toBytes(putRecord._2))
+                  table.put(put)
+                })
 
       }
     }
@@ -121,3 +119,6 @@ object Json {
     ssc.awaitTermination()
   }
 }
+
+Json.main(null)
+
