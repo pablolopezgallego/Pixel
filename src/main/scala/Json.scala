0847f792c2be422be794829e1645a8beea0a1688
pablolopezgallego <36295104+pablolopezgallego@users.noreply.github.com>
2018-02-21 13:08:34 +0100

    Primera subida

--- src/main/scala/Json.scala
+++ src/main/scala/Json.scala
@@ -1,116 +1,180 @@
-/*
-import _root_.kafka.serializer.{DefaultDecoder, StringDecoder}
-import org.apache.hadoop.hbase.client.{ConnectionFactory, Get}
-import org.apache.hadoop.hbase.mapreduce.{TableInputFormat}
-import org.apache.hadoop.hbase.TableName
-import org.apache.hadoop.hbase.client._
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.storage.StorageLevel
+import java.text.SimpleDateFormat
+import java.util.HashMap
+import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
+import org.apache.spark.SparkConf
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.hbase.client.{Get, ConnectionFactory}
+import org.apache.hadoop.hbase.io.hfile.{CacheConfig, HFile}
+import org.apache.hadoop.hbase.mapreduce.LoadIncrementalHFiles
+//import org.apache.hadoop.hbase.{HConstants, CellUtil, HBaseTestingUtility, TableName}
 import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.spark.HBaseDStreamFunctions._
 import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.hadoop.hbase.spark.HBaseContext
+import org.apache.spark.{SparkContext, Logging}
+import org.junit.rules.TemporaryFolder
+import org.apache.hadoop.hbase.spark._
+import org.apache.hadoop.hbase.TableName
+import org.apache.hadoop.hbase.client._
+import org.apache.hadoop.hbase.exceptions._
+import org.apache.hadoop.hbase.CellComparator
+import _root_.kafka.serializer.{DefaultDecoder, StringDecoder}
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
+import org.apache.hadoop.hbase.client.{ConnectionFactory, _}
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.hadoop.hbase.spark.HBaseContext
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.client.Increment
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.kafka._
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 
-object Json {
+object streaming extends Serializable {
+  implicit val formats = DefaultFormats
+  var HBASE_DB_HOST: String = null
+  var HBASE_TABLE: String = null
+  var HBASE_COLUMN_FAMILY: String = null
+
+  case class adn(idTracker: String, decay: String, url: String, ip: String, useragent: String, os: String, dispositivo: String, language: String)
+
   //  org.apache.log4j.BasicConfigurator.configure()
   def main(args: Array[String]) {
-
     /** EL código de spark conf para hacer el streaming */
+    /*
+            val conf = new SparkConf().setAppName("HBaseStream")
+            if (sys.env("ENTORNO") == "DESARROLLO") {
+              conf.setMaster("local[*]")
+            }
+    */
+    val ssc = new StreamingContext(sc, Seconds(1))
+    //val sc = ssc.sparkContext
 
-    val conf = new SparkConf().setAppName("HBaseStream")
-    if (sys.env("ENTORNO") == "DESARROLLO") {
-      conf.setMaster("local[*]")
-    }
-
-    val ssc = new StreamingContext(conf, Seconds(10))
-
-    /* KafkaConf tiene un Map de la ruta del server de kafka, la ruta del server de zookeeper, el grupo.id del consumidor para poder hacer redundancia, el timeout para conectar a zookeeper */
+    /** KafkaConf tiene un Map de la ruta del server de kafka, la ruta del server de zookeeper, el grupo.id del consumidor para poder hacer redundancia, el timeout para conectar a zookeeper */
     val kafkaConf = Map("metadata.broker.list" -> "localhost:42111",
       "zookeeper.connect" -> "localhost:21000",
-      "group.id" -> "pablo",
+      "group.id" -> "kafka-example",
       "zookeeper.connection.timeout.ms" -> "1000",
       "zookeeper.session.timeout.ms" -> "10000")
 
-    /* Los valores que recibe kafka son 4, un streaming context, el kafkaCOnf, un Map del tópic transformado en integer y el cuarto es como guardar los datos, según este ejemplo solo cojo el value (la key no) */
+    /** Los valores que recibe kafka son 4, un streaming context, el kafkaCOnf, un Map del tópic transformado en integer y el cuarto es como guardar los datos, según este ejemplo solo cojo el value (la key no) */
     val lines = KafkaUtils.createStream[Array[Byte], String, DefaultDecoder, StringDecoder](
       ssc, kafkaConf, Map("test" -> 1),
       StorageLevel.MEMORY_ONLY_SER).map(_._2)
 
-    /* Creamos el SqlContext para usar Json */
-        val sc = ssc.sparkContext
-    val SqlContext = new org.apache.spark.sql.SQLContext(sc)
-
-    /*Importamos taxonomías
-    val rutaTax = "file:///C:/Users/plopez/Desktop/Taxonomias.csv" //URL a identificador
-    val camposTax = "file:///C:/Users/plopez/Desktop/DictTax.csv" //identificador a taxoniomía
-    val camposGenerales = "file:///C:/Users/plopez/Desktop/dictVarSanitas.txt" //Campos que contiene el Json recibido
-*/
-    /* Versión en el servidor, en un futuro se pasará como argumentos */
     val rutaTax = "file:///Pablo/Taxonomias.csv" //URL a identificador
-    val camposTax = "file:///Pablo/DictTax.csv" //identificador a taxoniomía
+    //val camposTax = "file:///Pablo/DictTax.csv" //identificador a taxoniomía
     val camposGenerales = "file:///Pablo/dictVarSanitas.txt" //Campos que contiene el Json recibido
     //val rutaTrafico = args(3)    No se usa, la ruta es lo que leemos de kafka, seguramente sea el topic
-    //val destino = args(4)  Es HBase, aún desconocemos que meteremos al final, seguramente el HBase column adn y tabla
-
-    val tax = sc.textFile(rutaTax, 1)
+    //val destino = args(4)  Es HBase, aún desconocemos que meteremos al final, seguramente el HBase colum adn y tabla
+    val tax = sc.textFile(rutaTax)
     val taxFM = tax.map(x => (x.split(";")(0), x.split(";")(1)))
-    val camposTaxRDD = sc.textFile(camposTax, 1)
-    val camposGeneralesRDD = sc.textFile(camposGenerales, 1)
+    //val camposTaxRDD = sc.textFile(camposTax)
+    val camposGeneralesRDD = sc.textFile(camposGenerales)
+    //val mapaTax = camposTaxRDD.map(x => (x.split(";")(0), 0)).collect()
+    val mapaVarGen: Array[String] = camposGeneralesRDD.map(x => x.replaceAll("parametros.", "")).collect
+    //lines.print()
 
-    val mapaTax = camposTaxRDD.map(x => (x.split(";")(0), 0)).collect()
-    val mapaVarGen = camposGeneralesRDD.map(x => x.replaceAll("parametros.", "").collect
+    /** Trasformamos la linea de entrada en un Json */
+    //    val traficoRDD = lines.map(x => (compact(parse(x) \ "url").replaceAll("\"", ""), Array(for (y<-mapaVarGen) compact(parse(x) \ y).toString.replaceAll("\"", ""))))
+    //val traficoRDD: DStream[(String, Array[String])] = lines.map(x => (compact(parse(x) \ "url").replaceAll("\"", ""), Array(compact(parse(x) \ "idTracker"), compact(parse(x) \ "decay"), compact(parse(x) \ "ip"), compact(parse(x) \ "useragent"), compact(parse(x) \ "os"), compact(parse(x) \ "dispositivo"), compact(parse(x) \ "language"))))
+    val traficoRD: DStream[Array[String]] = lines.map(x =>Array(compact(parse(x) \ "idTracker"), compact(parse(x) \ "decay"), compact(parse(x) \ "ip"), compact(parse(x) \ "useragent"), compact(parse(x) \ "os"), compact(parse(x) \ "dispositivo"), compact(parse(x) \ "language")))
 
-    //mapaTax.foreach(println)
+    //traficoRDD.foreachRDD(x=>x.foreach(y=>print("Hola"+y._2)))
+    //val traficoTax: DStream[Array[String]] = traficoRDD.transform(rdd => rdd.join(taxFM).map(x => Array(x._2._2.toString).union(x._2._1))) //.union(x._2._2.toString)))
+    //traficoTax.print()
 
-    sc.setLogLevel("ERROR")
-    val config = HBaseConfiguration.create()
-    val hbaseContext = new HBaseContext(sc, config)
-    ssc.checkpoint("/tmp/spark_checkpoint")
+    /** Inicializamos los valores de HBase */
+    HBASE_DB_HOST = "127.0.0.1"
+    HBASE_TABLE = "usuarios"
+    HBASE_COLUMN_FAMILY = "adn"
+    //    ssc.checkpoint("/tmp/spark_checkpoint")
+    //traficoTax.foreachRDD { rdd => rdd.foreachPartition(x => putHBase(x, mapaVarGen)) }
+    //traficoTax.foreachRDD(aux => {for (x <- aux.collect) putHBase(x, mapaVarGen)})
+    putHBase(traficoRD, mapaVarGen)
 
-//    lines.print()
-//    val trafico = lines.flatMap(k=>k.slice(k.lastIndexOf("idTracker")+1,k.indexOf("url")-2))
-//    trafico.foreachRDD(k=>println(k.collect()(0) + k.collect()(1) ))
-
-    lines.foreachRDD { k =>
-      if (k.count() > 0) {
-        /*Traducimos el Json a RDD */
-        val traficoRDD = SqlContext.read.json(k).selectExpr(List("idTracker", "decay", "url") ++ camposGeneralesRDD.collect(): _*).rdd.keyBy(t => if (t.getAs[String]("url").indexOf('?') > 0) t.getAs[String]("url").substring(0, t.getAs[String]("url").indexOf('?')) else t.getAs[String]("url"))
-        var traficoTax = traficoRDD.join(taxFM).map(x => (x._2._1.getAs("idTracker").toString(), x._2))
-
-        val APP_NAME: String = "SparkHbaseJob"
-        var HBASE_DB_HOST: String = null
-        var HBASE_TABLE: String = null
-        var HBASE_COLUMN_FAMILY: String = null
-        HBASE_DB_HOST = "127.0.0.1"
-        HBASE_TABLE = "usuarios"
-        HBASE_COLUMN_FAMILY = "adn"
-        val conf = HBaseConfiguration.create()
-        conf.set(TableInputFormat.INPUT_TABLE, HBASE_TABLE)
-        val connection = ConnectionFactory.createConnection(conf)
-        val table = connection.getTable(TableName.valueOf(Bytes.toBytes(HBASE_TABLE))) //nombre tabla
-
-        var csv = traficoTax.map(l => (l._2._1, l._2._2))
-        //traficoTax.collect().foreach(l => println { "traficoTax: " + l})
-        //traficoRDD.collect().foreach(l => println {"traficoRDD: " + l})
-        csv.collect().foreach(l => println {"csv: " + l})
-
-        csv.collect().foreach( aux => {
-        //            for (aux<-csv){
-            val p = new Put(Bytes.toBytes(aux._1.getAs("idTracker").toString()))
-            for ((key,v) <- mapaVarGen) {
-              p.addColumn( Bytes.toBytes("adn"),  Bytes.toBytes(""+key), aux._1.getAs("decay"), Bytes.toBytes(aux._1.getAs(""+key).toString))
-              table.put(p)
-            }
-            val inc = new Increment(Bytes.toBytes(aux._1.getAs("idTracker").toString())) //key
-            inc.addColumn(Bytes.toBytes("adn"), Bytes.toBytes("idTax"+aux._2.toString), 1) //ColFamily,col,incremento
-            table.increment(inc)
-          })
-
-      }
+    //def putHBase(partitions: Iterator[(Array[String])], mapaVarGen: (Array[String])) {
+    def putHBase(partitions: DStream[(Array[String])], mapaVarGen: (Array[String])) {
+      sc.setLogLevel("ERROR")
+      val config = new HBaseConfiguration()
+      val hbaseContext = new HBaseContext(sc, config)
+      ssc.checkpoint("/tmp/spark_checkpoint")
+      //      if (!partitions.isEmpty) {
+      partitions.hbaseBulkPut(
+        hbaseContext,
+        TableName.valueOf("b"),
+        (row) => {
+          //.next()
+          //POR CADA COLUMNA REALIZO UN PUT
+          val put = new Put(Bytes.toBytes("b")) //idTracker
+          put.addColumn(Bytes.toBytes("b"), Bytes.toBytes("b"), Bytes.toBytes("b")) //colFamily,col,timestamp,value
+          put
+          /*
+                  val inc = new Increment(Bytes.toBytes(row(1).toString)) //idTracker
+                  inc.addColumn(Bytes.toBytes("adn"), Bytes.toBytes("idTax" + row(0).toString), 1) //colFamily,col,timestamp,value
+                  table.increment(inc)
+                  */
+        })
     }
+    //    }
     ssc.start()
     ssc.awaitTermination()
   }
 }
-Json.main(null)
-*/
+streaming.main(null)
+
+
+import java.util.HashMap
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka._
+import org.apache.spark.SparkConf
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.hbase.client.{Get, ConnectionFactory}
+import org.apache.hadoop.hbase.io.hfile.{CacheConfig, HFile}
+import org.apache.hadoop.hbase.mapreduce.LoadIncrementalHFiles
+import org.apache.hadoop.hbase.{HConstants, CellUtil, HBaseTestingUtility, TableName}
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.spark.HBaseDStreamFunctions._
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.spark.HBaseContext
+import org.apache.spark.{SparkContext, Logging}
+import org.junit.rules.TemporaryFolder
+import org.apache.hadoop.hbase.spark._
+import org.apache.hadoop.hbase.TableName
+import org.apache.hadoop.hbase.client._
+import org.apache.hadoop.hbase.exceptions._
+import org.apache.hadoop.hbase.CellComparator
+
+sc.setLogLevel("ERROR")
+val config = new HBaseConfiguration()
+val hbaseContext = new HBaseContext(sc, config)
+val ssc = new StreamingContext(sc, Seconds(10))
+ssc.checkpoint("/tmp/spark_checkpoint")
+
+
+val lines = KafkaUtils.createStream(ssc, "localhost:21000", "grupo", Map(("test",1))).map(_._2)
+val csv = lines.map(x=>x.split(","))
+//144,ALICANTE,20170117,4.8,7.2
+csv.hbaseBulkPut(
+  hbaseContext,
+  TableName.valueOf("meteoHBase"),
+  (putRecord) => {
+    val put = new Put(Bytes.toBytes(putRecord(0)))
+    put.addColumn( Bytes.toBytes("datos"),  Bytes.toBytes("provincia"), Bytes.toBytes(putRecord(1)))
+    put.addColumn( Bytes.toBytes("datos"),  Bytes.toBytes("fecha"), Bytes.toBytes(putRecord(2)))
+    put.addColumn( Bytes.toBytes("datos"),  Bytes.toBytes("precipitacion"), Bytes.toBytes(putRecord(3)))
+    put.addColumn( Bytes.toBytes("datos"),  Bytes.toBytes("temperatura"), Bytes.toBytes(putRecord(4)))
+    put
+  })
+ssc.start()
+ssc.awaitTermination()
--- src/main/scala/streaming.scala
+++ src/main/scala/streaming.scala
@@ -1,11 +1,22 @@
 import java.text.SimpleDateFormat
-import _root_.kafka.serializer.{DefaultDecoder, StringDecoder}
-import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
+import java.util.HashMap
+import org.junit.rules.TemporaryFolder
+import org.apache.hadoop.hbase.spark._
+import org.apache.hadoop.hbase.TableName
+import org.apache.hadoop.hbase.client._
+import org.apache.hadoop.hbase.exceptions._
+import org.apache.hadoop.hbase.CellComparator
 import org.apache.hadoop.hbase.client.{ConnectionFactory, _}
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.hadoop.hbase.client.Increment
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+
+import _root_.kafka.serializer.{DefaultDecoder, StringDecoder}
+//import org.apache.hadoop.hbase.{HConstants, CellUtil, HBaseTestingUtility, TableName}
+import org.apache.hadoop.hbase.spark.HBaseDStreamFunctions._
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
 import org.apache.hadoop.hbase.spark.HBaseContext
 import org.apache.hadoop.hbase.util.Bytes
-import org.apache.hadoop.hbase.client.Increment
 import org.apache.spark.SparkConf
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.dstream.DStream
@@ -25,14 +36,14 @@ object streaming extends Serializable {
   //  org.apache.log4j.BasicConfigurator.configure()
   def main(args: Array[String]) {
     /** EL código de spark conf para hacer el streaming */
-    /*
-            val conf = new SparkConf().setAppName("HBaseStream")
-            if (sys.env("ENTORNO") == "DESARROLLO") {
-              conf.setMaster("local[*]")
-            }
-    */
-    val ssc = new StreamingContext(sc, Seconds(1))
-    //val sc = ssc.sparkContext
+
+    val conf = new SparkConf().setAppName("HBaseStream")
+    if (sys.env("ENTORNO") == "DESARROLLO") {
+      conf.setMaster("local[*]")
+    }
+
+    val ssc = new StreamingContext(conf, Seconds(1))
+    val sc = ssc.sparkContext
 
     /** KafkaConf tiene un Map de la ruta del server de kafka, la ruta del server de zookeeper, el grupo.id del consumidor para poder hacer redundancia, el timeout para conectar a zookeeper */
     val kafkaConf = Map("metadata.broker.list" -> "localhost:42111",
@@ -46,58 +57,66 @@ object streaming extends Serializable {
       ssc, kafkaConf, Map("test" -> 1),
       StorageLevel.MEMORY_ONLY_SER).map(_._2)
 
-    val rutaTax = "file:///Pablo/Taxonomias.csv" //URL a identificador
-    //val camposTax = "file:///Pablo/DictTax.csv" //identificador a taxoniomía
-    val camposGenerales = "file:///Pablo/dictVarSanitas.txt" //Campos que contiene el Json recibido
-    //val rutaTrafico = args(3)    No se usa, la ruta es lo que leemos de kafka, seguramente sea el topic
-    //val destino = args(4)  Es HBase, aún desconocemos que meteremos al final, seguramente el HBase colum adn y tabla
-    val tax = sc.textFile(rutaTax)
-    val taxFM = tax.map(x => (x.split(";")(0), x.split(";")(1)))
-    //val camposTaxRDD = sc.textFile(camposTax)
-    val camposGeneralesRDD = sc.textFile(camposGenerales)
-    //val mapaTax = camposTaxRDD.map(x => (x.split(";")(0), 0)).collect()
-    val mapaVarGen: Array[String] = camposGeneralesRDD.map(x => x.replaceAll("parametros.", "")).collect
-    //lines.print()
+      val rutaTax = "file:///Pablo/Taxonomias.csv" //URL a identificador
+      //val camposTax = "file:///Pablo/DictTax.csv" //identificador a taxoniomía
+      val camposGenerales = "file:///Pablo/dictVarSanitas.txt" //Campos que contiene el Json recibido
+      //val rutaTrafico = args(3)    No se usa, la ruta es lo que leemos de kafka, seguramente sea el topic
+      //val destino = args(4)  Es HBase, aún desconocemos que meteremos al final, seguramente el HBase colum adn y tabla
+      val tax = sc.textFile(rutaTax)
+      val taxFM = tax.map(x => (x.split(";")(0), x.split(";")(1)))
+      //val camposTaxRDD = sc.textFile(camposTax)
+      val camposGeneralesRDD = sc.textFile(camposGenerales)
+      //val mapaTax = camposTaxRDD.map(x => (x.split(";")(0), 0)).collect()
+      val mapaVarGen: Array[String] = camposGeneralesRDD.map(x => x.replaceAll("parametros.", "")).collect
+      //lines.print()
 
-    /** Trasformamos la linea de entrada en un Json */
-    //    val traficoRDD = lines.map(x => (compact(parse(x) \ "url").replaceAll("\"", ""), Array(for (y<-mapaVarGen) compact(parse(x) \ y).toString.replaceAll("\"", ""))))
-    val traficoRDD: DStream[(String, Array[String])] = lines.map(x => (compact(parse(x) \ "url").replaceAll("\"", ""), Array(compact(parse(x) \ "idTracker"), compact(parse(x) \ "decay"), compact(parse(x) \ "ip"), compact(parse(x) \ "useragent"), compact(parse(x) \ "os"), compact(parse(x) \ "dispositivo"), compact(parse(x) \ "language"))))
-    //traficoRDD.foreachRDD(x=>x.foreach(y=>print("Hola"+y._2)))
-    val traficoTax: DStream[Array[String]] = traficoRDD.transform(rdd => rdd.join(taxFM).map(x => Array(x._2._2.toString).union(x._2._1))) //.union(x._2._2.toString)))
-    //traficoTax.print()
+      /** Trasformamos la linea de entrada en un Json */
+      //val traficoRDD = lines.map(x => (compact(parse(x) \ "url").replaceAll("\"", ""), Array(for (y<-mapaVarGen) compact(parse(x) \ y).toString.replaceAll("\"", ""))))
+      val traficoRDD: DStream[(String, (String, Array[String]))] = lines.map(x => (compact(parse(x) \ "url").replaceAll("\"", ""), (compact(parse(x) \ "idTracker").replaceAll("\"", "") + compact(parse(x) \ "decay").replaceAll("\"", ""), Array(compact(parse(x) \ "ip").replaceAll("\"", ""), compact(parse(x) \ "useragent").replaceAll("\"", ""), compact(parse(x) \ "os").replaceAll("\"", ""), compact(parse(x) \ "dispositivo").replaceAll("\"", ""), compact(parse(x) \ "language").replaceAll("\"", "")))))
+      val traficoTax: DStream[(String, Array[String], Array[String])] = traficoRDD.transform(rdd => rdd.join(taxFM).map(x => (x._2._1._1, Array(x._2._2.toString).union(x._2._1._2), Array("idTax"+x._2._2.toString).union(mapaVarGen))))
+      //val traficoFinal: DStream[(String, Array[String], Array[String])] = traficoTax.map(x => (x._1, x._2, Array("idTax").union(mapaVarGen)))
 
-    /** Inicializamos los valores de HBase */
-    HBASE_DB_HOST = "127.0.0.1"
-    HBASE_TABLE = "usuarios"
-    HBASE_COLUMN_FAMILY = "adn"
-    val confi = HBaseConfiguration.create()
-    confi.set(TableInputFormat.INPUT_TABLE, HBASE_TABLE)
-    val connection = ConnectionFactory.createConnection(confi)
-    val table = connection.getTable(TableName.valueOf(Bytes.toBytes(HBASE_TABLE)))
-    //    ssc.checkpoint("/tmp/spark_checkpoint")
-//    traficoTax.foreachRDD { rdd => rdd.foreachPartition(x => putHBase(x, mapaVarGen)) }
-    traficoTax.foreachRDD(aux => {for (x <- aux.collect) putHBase(x, mapaVarGen)})
-    table.close()
-    connection.close()
+    putHBase(traficoTax)
+
+      /** Inicializamos los valores de HBase */
+      HBASE_DB_HOST = "127.0.0.1"
+      HBASE_TABLE = "usuarios"
+      HBASE_COLUMN_FAMILY = "adn"
+      //    ssc.checkpoint("/tmp/spark_checkpoint")
+      //traficoTax.foreachRDD { rdd => rdd.foreachPartition(x => putHBase(x, mapaVarGen)) }
+      //traficoTax.foreachRDD(aux => {for (x <- aux.collect) putHBase(x, mapaVarGen)})
+
     //def putHBase(partitions: Iterator[(Array[String])], mapaVarGen: (Array[String])) {
-    def putHBase(partitions: (Array[String]), mapaVarGen: (Array[String])) {
-//      if (!partitions.isEmpty) {
-        val row = partitions//.next()
-        //POR CADA COLUMNA REALIZO UN PUT
-        val put = new Put(Bytes.toBytes(row(1).toString)) //idTracker
-        for (j <- 0 until mapaVarGen.length - 1) {
-          put.addColumn(Bytes.toBytes("adn"), Bytes.toBytes("" + mapaVarGen(j)), Bytes.toBytes(row(j + 3).toString)) //colFamily,col,timestamp,value
-          table.put(put)
-        }
+    def putHBase(partitions: DStream[(String, Array[String], Array[String])]) {
+      sc.setLogLevel("ERROR")
+      val config = new HBaseConfiguration()
+      val hbaseContext = new HBaseContext(sc, config)
+      ssc.checkpoint("/tmp/spark_checkpoint")
+      //      if (!partitions.isEmpty) {
+
+      partitions.hbaseBulkPut(
+        hbaseContext,
+        TableName.valueOf("usuarios"),
+        (row) => {
+          //POR CADA COLUMNA REALIZO UN PUT
+          val put = new Put(Bytes.toBytes(row._1)) //idTracker
+          for (x <- 0 until row._2.length) {
+            put.addColumn(Bytes.toBytes("adn"), Bytes.toBytes(row._3(x)), Bytes.toBytes(row._2(x))) //colFamily,col,timestamp,value
+          }
+          put
+          /*
         val inc = new Increment(Bytes.toBytes(row(1).toString)) //idTracker
         inc.addColumn(Bytes.toBytes("adn"), Bytes.toBytes("idTax" + row(0).toString), 1) //colFamily,col,timestamp,value
         table.increment(inc)
-      }
-//    }
+        */
+        })
+    }
+    //    }
     ssc.start()
     ssc.awaitTermination()
   }
 }
+
 streaming.main(null)
 
 
